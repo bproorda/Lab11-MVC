3610052448eb94c458f9979014aaafd83cb57ab2
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-18 18:59:43 -0500

    screwed up, setting up  repo

---
+++ Lab_11_MVC/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Lab_11_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_11_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int StartYear, int EndYear)
+        {
+            // Don't show View() directly in a POST; redirect instead
+            // return View();
+
+            // Post - Redirect - Get
+            return RedirectToAction("Adder", new { StartYear, EndYear });
+        }
+
+        // Model binding of all the applicable properties in the model type
+        public IActionResult Adder(IndexViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+    }
+}
