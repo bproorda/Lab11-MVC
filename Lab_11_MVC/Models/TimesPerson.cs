2845d30e59f7736615491dc08a40f54f4fc0cd10
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-18 19:05:20 -0500

    added Times Person Class

--- Lab_11_MVC/Models/TimesPerson.cs
+++ Lab_11_MVC/Models/TimesPerson.cs
@@ -8,7 +8,39 @@ namespace Lab_11_MVC.Models
 {
     public class TimesPerson
     {
-    
+		public int StartYear { get; set; }
+		public int EndYear { get; set; }
+		public int Year { get; set; }
+		public string Honor { get; set; }
+		public string Name { get; set; }
+		public string Country { get; set; }
+		public int BirthYear { get; set; }
+		public int DeathYear { get; set; }
+		public string Title { get; set; }
+		public string Category { get; set; }
+		public string Context { get; set; }
+
+		public List<TimesPerson> GetTimesPeople()
+		{
+			string[] people = File.ReadAllLines("Data/personOfTheYear.csv");
+
+			return people
+				.Skip(1)
+				.Select(personLine => personLine.Split(","))
+				.Select(personCell => new TimesPerson()
+				{
+					Year = int.Parse(personCell[0]),
+					Honor = personCell[1],
+					Name = personCell[2],
+					Country = personCell[3],
+					BirthYear = int.Parse(personCell[4]),
+					DeathYear = int.Parse(personCell[5]),
+					Title = personCell[6],
+					Category = personCell[7],
+					Context = personCell[8],
+				}).ToList();
+		}
+
 
 	}
 }
